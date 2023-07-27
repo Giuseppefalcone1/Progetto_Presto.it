36b091734c3607d750910e14190d3d04e2087007
Giuseppe Falcone <giuseppe.falcone1@hotmail.com>
2023-07-27 18:25:50 +0200

    corretti alcuni bug e inizializzata sezione con numeri incrementale

--- main.js
+++ main.js
@@ -4,17 +4,13 @@ let links = document.querySelectorAll(".links");
 
 let divimg1 = document.querySelector("#divImmagine1");
 
-// console.dir(window);
-
 window.addEventListener(`scroll` , ()=>{
-
-    // function scroll_Nav(){}
-
+ 
     let scrolled = window.scrollY;
 
     if(scrolled > 0){
 
-        navbarMain.classList.add("navbar-scroll");
+        navbarMain.classList.add("navbarRed");
         links.forEach((link)=>{
             link.style.color = "var(--yellowCustom)";
         
@@ -22,21 +18,82 @@ window.addEventListener(`scroll` , ()=>{
 
     }else{
 
-        navbarMain.classList.remove("navbar-scroll");
-
+        navbarMain.classList.remove("navbarRed");
         links.forEach((link)=>{
             link.style.color = "white";
+        
         })
+        
     };
 
-
-    // if (scroll > 1){
-
-    //     divimg1.classList.add("animate__animated, animate__fadeInRightBig")
-
-    // }
-
 });
+
+let number1 = querySelector("#number1")
+
+let number2 = querySelector("#number2")
+
+let number3 = querySelector("#number3")
+
+let counter = 0
+
+let interval = setInterval(()=>{
+    
+    if(counter < 100){
+
+        counter++
+
+        number1.innerHTML = counter ;
+
+    }
+
+}100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeNav(nameNavbarClass, linkColorDefault, linkColorScroll){
+
+//     let navbarMain = document.querySelector("#navbarMain");
+
+//     let links = document.querySelectorAll(".links");
+    
+//     let scrolled = window.scrollY;
+//     if(scrolled > 0){
+
+//         navbarMain.classList.add("nameNavbarClass");
+//         links.forEach((link)=>{
+//             link.style.color = "linkColorScroll";
+        
+//         })
+
+//     }else{
+
+//         navbarMain.classList.remove("nameNavbarClass");
+//         links.forEach((link)=>{
+//             link.style.color = "linkColorDefault";
+        
+//         })
+        
+//     };
+
+// } 
+
+// changeNav(navbarRed, var(--yellowCustom), white);
+
 
 
 
